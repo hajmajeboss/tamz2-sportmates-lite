7e87e38ed17a0a95e3813e656244288038355448
Petr Heinz <p.heinz@email.cz>
2018-11-28 14:42:08 +0100

    firebase setup

--- src/main/java/cz/greapp/sportmateslite/Data/TableGateways/TableGateway.java
+++ src/main/java/cz/greapp/sportmateslite/Data/TableGateways/TableGateway.java
@@ -1,4 +1,12 @@
 package cz.greapp.sportmateslite.Data.TableGateways;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class TableGateway {
+
+    protected FirebaseFirestore db;
+
+    public TableGateway() {
+        db = FirebaseFirestore.getInstance();
+    }
 }
---
+++ src/main/java/cz/greapp/sportmateslite/Data/TableGateways/UserTableGateway.java
@@ -0,0 +1,26 @@
+package cz.greapp.sportmateslite.Data.TableGateways;
+
+public class UserTableGateway extends TableGateway {
+
+    public UserTableGateway() {
+        super();
+    }
+
+    public void getUser() {
+
+    }
+
+    public void putUser() {
+
+    }
+
+    public void deleteUser() {
+
+    }
+
+    public void updateUser() {
+
+    }
+
+
+}
